d99a19dff908e02a79ccc4935a51e820b6fe54b1
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-16 11:05:06 +0900

    menu

---
+++ resources/views/menu/order.blade.php
@@ -0,0 +1,6 @@
+<div class="py-3">
+    <h3 class="font-bold text-lg">飲食店向けのセルフオーダーシステム</h3>
+
+    <div>スマホで注文するシステム。店内での利用。</div>
+
+</div>
---
+++ resources/views/menu/system.blade.php
@@ -0,0 +1,6 @@
+<div class="py-3">
+    <h3 class="font-bold text-lg">Webシステム開発</h3>
+
+    <div>SDSサイトやここのようなプログラマー／エンジニアによるWebシステム開発。</div>
+
+</div>
