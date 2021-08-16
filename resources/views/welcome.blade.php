6e7d69688a5e6a14feb0cc850aa62948fd43ca8a
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-16 12:00:33 +0900

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,43 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
-    <div class="mt-2 text-2xl">
+    <h1 class="mt-2 text-2xl">
         サブカルデザインスタジオ福岡への仕事の依頼はこちらから
-    </div>
+    </h1>
 
     <div class="mt-6 text-gray-500">
-        ...
+        <div>...</div>
+
+        <div class="py-3">
+            問い合わせは
+            <a href="http://www.sds.fukuoka.jp/" class="text-indigo-500 font-bold underline"
+                target="_blank">SDSサイトの問い合わせフォーム</a>、
+            <a href="https://twitter.com/sdsfukuoka" class="text-indigo-500 font-bold underline"
+                target="_blank">Twitter</a>、
+            等から受け付けています。
+        </div>
     </div>
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">仕事メニュー</div>
+            <h2 class="ml-4 text-lg text-gray-600 leading-7 font-semibold">仕事メニュー</h2>
         </div>
 
         <div class="ml-12">
             <div class="mt-2 text-sm text-gray-500">
                 <div>こんなことができますという例。</div>
-<div class="divide-y">
-                @include('menu.system')
-                @include('menu.order')
-            </div>
+                <div class="divide-y">
+                    @include('menu.system')
+                    @include('menu.order')
+                </div>
             </div>
         </div>
     </div>
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold">制作実績</div>
+            <h2 class="ml-4 text-lg text-gray-600 leading-7 font-semibold">制作実績</h2>
         </div>
 
         <div class="ml-12">
