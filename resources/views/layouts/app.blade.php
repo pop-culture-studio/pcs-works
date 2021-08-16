5b59d9cf9e7c2f8a1ef1c8920f3a0710ca4be08c
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-16 17:37:07 +0900

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="サブカルデザインスタジオ福岡への仕事の依頼はこちらから">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
