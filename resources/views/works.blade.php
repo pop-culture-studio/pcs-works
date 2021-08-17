1c3792fc742c9252b1e5761e588148cbee02daeb
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-17 11:11:32 +0900

    Add youtube

--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -1,7 +1,10 @@
 <div class="mt-2 text-sm text-gray-500">
-    ...
+    <div>...</div>
+
     <div class="divide-y">
+        @include('works.youtube')
         @include('works.base-shop')
+
         @include('works.sample')
         @include('works.sample2')
     </div>
