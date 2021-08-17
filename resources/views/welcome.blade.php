909820f82a3768c42f4aaebfba075860eedc8976
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-17 11:42:19 +0900

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
-    <h1 class="mt-2 text-2xl">
+    <h1 class="mt-2 text-2xl font-extrabold">
         サブカルデザインスタジオ福岡(SDS)への仕事の依頼はこちらから
     </h1>
 
@@ -23,7 +23,7 @@
 <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
     <div class="p-6">
         <div class="flex items-center">
-            <h2 class="ml-1 sm:ml-4 text-lg text-gray-600 leading-7 font-semibold">仕事メニュー</h2>
+            <h2 class="ml-1 sm:ml-4 text-lg text-gray-600 leading-7 font-bold">仕事メニュー</h2>
         </div>
 
         <div class="ml-12">
@@ -33,7 +33,7 @@
 
     <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
         <div class="flex items-center">
-            <h2 class="ml-1 sm:ml-4 text-lg text-gray-600 leading-7 font-semibold">制作実績</h2>
+            <h2 class="ml-1 sm:ml-4 text-lg text-gray-600 leading-7 font-bold">制作実績</h2>
         </div>
 
         <div class="ml-12">
