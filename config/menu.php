02c5eba5d31aedc58c65bea06a59c057589a6a5f
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-17 14:51:07 +0900

    wip

--- config/menu.php
+++ config/menu.php
@@ -10,8 +10,8 @@ return [
         'text' => 'SDSサイトやここのようなプログラマー／エンジニアによるWebシステム開発。<a href="https://github.com/sds-fukuoka" class="underline" target="_blank">GitHub</a>',
     ],
     [
-        'title' => '飲食店向けのセルフオーダーシステム',
-        'text' => 'スマホで注文するシステム。店内で利用する想定。<a href="https://sds-order-sample.vercel.app/order/1" class="underline" target="_blank">サンプル</a>',
+        'title' => '飲食店向けのスマホで注文するシステム',
+        'text' => '店内で利用する想定。<a href="https://sds-order-sample.vercel.app/order/1" class="underline" target="_blank">サンプル</a>',
     ],
     [
         'title' => '小さい通知ツール',
