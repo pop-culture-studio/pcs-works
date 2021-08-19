9a55c5f53e1a2f7e056584ee026ae41130b6583f
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-19 14:55:24 +0900

    contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<div class="py-3">
+    問い合わせは
+    <a href="https://sds.fukuoka.jp/" class="text-indigo-500 font-bold underline"
+        target="_blank">SDS公式サイトの問い合わせフォーム</a>、
+    <a href="https://twitter.com/sdsfukuoka" class="text-indigo-500 font-bold underline"
+        target="_blank">Twitter</a>、
+    <a href="https://www.instagram.com/sds_nozamaru/" class="text-indigo-500 font-bold underline"
+        target="_blank">Instagram</a>、
+    <a href="tel:0922925417" class="text-indigo-500 font-bold underline"
+        target="_blank">電話 092-292-5417</a>
+    等から受け付けています。
+</div>
