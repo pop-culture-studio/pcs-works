a1dbd17015bccce3209f928154004f6eaceca14d
sds-fukuoka <sds.engineer.team@gmail.com>
2021-08-21 13:23:22 +0900

    tw

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,7 +2,7 @@
     問い合わせは
     <a href="https://sds.fukuoka.jp/" class="text-indigo-500 font-bold underline"
         target="_blank">SDS公式サイトの問い合わせフォーム</a>、
-    <a href="https://twitter.com/sdsfukuoka" class="text-indigo-500 font-bold underline"
+    <a href="https://twitter.com/SDSfukuoka" class="text-indigo-500 font-bold underline"
         target="_blank">Twitter@SDSfukuoka</a>、
     <a href="https://www.instagram.com/sds_nozamaru/" class="text-indigo-500 font-bold underline"
         target="_blank">Instagram</a>、
