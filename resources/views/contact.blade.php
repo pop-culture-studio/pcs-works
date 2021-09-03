cb7458c18b8992f9b5e6e92c7ec74538ebf10bb4
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-09-03 12:29:34 +0900

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 <div class="py-3">
     問い合わせは
     <a href="https://sds.fukuoka.jp/" class="text-indigo-500 font-bold underline"
-        target="_blank">SDS公式サイトの問い合わせフォーム</a>、
+        target="_blank">PCS公式サイトの問い合わせフォーム</a>、
     <a href="https://twitter.com/SDSfukuoka" class="text-indigo-500 font-bold underline"
         target="_blank">Twitter@SDSfukuoka</a>、
     <a href="https://www.instagram.com/sds_nozamaru/" class="text-indigo-500 font-bold underline"
