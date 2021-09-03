e1a46b6d6eecb1d422761bf4fbfea14df69197e7
pop-culture-studio <pcs.engineer.team@gmail.com>
2021-09-03 12:29:12 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
 
     <h1 class="mt-2 text-2xl font-extrabold">
-        サブカルデザインスタジオ福岡(SDS)への仕事の依頼はこちらから
+        ポップカルチャースタジオ未来図(PCS)への仕事の依頼はこちらから
     </h1>
 
     <div class="mt-6 text-gray-500">
