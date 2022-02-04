aa6668ad12e7cb57601feae0d7992124e216c11f
PCS <pcs.engineer.team@gmail.com>
2022-02-04 18:30:49 +0900

    Create grouphome.blade.php

---
+++ resources/views/works/grouphome.blade.php
@@ -0,0 +1,7 @@
+<div class="py-3">
+    <h3 class="font-bold text-xl">
+        <a href="https://grouphome.guide/" target="_blank" class="text-indigo-500 underline underline-offset-2">障害者グループホームガイド</a>
+    </h3>
+
+    <div>グループホームの情報サイト。</div>
+</div>
