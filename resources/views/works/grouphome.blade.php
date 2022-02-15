a112551840c4dcf8ebfd3b423222befdd31bf708
PCS <pcs.engineer.team@gmail.com>
2022-02-15 15:40:08 +0900

    Update grouphome.blade.php

--- resources/views/works/grouphome.blade.php
+++ resources/views/works/grouphome.blade.php
@@ -3,5 +3,5 @@
         <a href="https://grouphome.guide/" target="_blank" class="text-indigo-500 underline underline-offset-2">障害者グループホームガイド</a>
     </h3>
 
-    <div>グループホームの情報サイト。</div>
+    <div>グループホーム情報の検索サイト。</div>
 </div>
