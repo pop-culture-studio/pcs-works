27b63e202233df7ec37d948be58d09732107e740
PCS <pcs.engineer.team@gmail.com>
2022-03-02 11:30:19 +0900

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,7 @@
 <div class="py-3 text-md">
     問い合わせは
-    <a href="https://sds.fukuoka.jp/" class="text-indigo-500 font-bold underline underline-offset-2"
-        target="_blank">PCS公式サイトの問い合わせフォーム</a>、
+{{--    <a href="https://sds.fukuoka.jp/" class="text-indigo-500 font-bold underline underline-offset-2"--}}
+{{--        target="_blank">PCS公式サイトの問い合わせフォーム</a>、--}}
     <a href="https://twitter.com/PCS_miraizu" class="text-indigo-500 font-bold underline underline-offset-2"
         target="_blank">Twitter@PCS_miraizu</a>、
     <a href="https://www.instagram.com/pcs_nozamaru/" class="text-indigo-500 font-bold underline underline-offset-2"
