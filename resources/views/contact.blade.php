4ae3d4b04e342ff8740e01ec0ea6872b8850a5bc
PCS <pcs.engineer.team@gmail.com>
2022-03-02 14:33:00 +0900

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,15 @@
 <div class="py-3 text-md">
     問い合わせは
-{{--    <a href="https://sds.fukuoka.jp/" class="text-indigo-500 font-bold underline underline-offset-2"--}}
-{{--        target="_blank">PCS公式サイトの問い合わせフォーム</a>、--}}
+    {{--    <a href="https://sds.fukuoka.jp/" class="text-indigo-500 font-bold underline underline-offset-2"--}}
+    {{--        target="_blank">PCS公式サイトの問い合わせフォーム</a>、--}}
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfWdOuysKuaNyOcNJ2e2VvXRApInKDq2GlXk8oJRYDOLQjcAA/viewform"
+       class="text-indigo-500 font-bold underline underline-offset-2"
+       target="_blank">問い合わせフォーム</a>、
     <a href="https://twitter.com/PCS_miraizu" class="text-indigo-500 font-bold underline underline-offset-2"
-        target="_blank">Twitter@PCS_miraizu</a>、
+       target="_blank">Twitter@PCS_miraizu</a>、
     <a href="https://www.instagram.com/pcs_nozamaru/" class="text-indigo-500 font-bold underline underline-offset-2"
-        target="_blank">Instagram</a>、
+       target="_blank">Instagram</a>、
     <a href="tel:0922925417" class="text-indigo-500 font-bold underline underline-offset-2"
-        target="_blank">電話 092-292-5417</a>
+       target="_blank">電話 092-292-5417</a>
     等から受け付けています。
 </div>
