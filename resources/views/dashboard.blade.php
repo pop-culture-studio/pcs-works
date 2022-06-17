8eb2a25ba635d1d4df33afee78e3043134c4ae81
PCS <pcs.engineer.team@gmail.com>
2022-06-17 18:28:38 +0900

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg
-            bg-fixed sm:bg-local
-            bg-cover sm:bg-contain
-            sm:bg-right
-            bg-no-repeat
-            bg-[url('/images/S__8962528.jpg')]">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @include('welcome')
             </div>
         </div>
