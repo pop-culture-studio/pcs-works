91e9136c28b4a8241d58e73c4125321e3533e67c
PCS <pcs.engineer.team@gmail.com>
2022-11-16 21:25:32 +0900

    Update menu.php

--- config/menu.php
+++ config/menu.php
@@ -10,7 +10,7 @@ return [
         'text' => '<a href="https://pcs-miraizu.com/" class="underline underline-offset-2" target="_blank">PCSサイト</a>やここのようなプログラマー／エンジニアによるWebシステム開発。<a href="https://github.com/pop-culture-studio" class="underline underline-offset-2" target="_blank">GitHub</a>
 <div class="bg-white border rounded my-2">
         <div class="border-b p-2"><span class="font-bold mr-3">料金目安</span>月額11万円(10万円+消費税1万円)</div>
-        <div class="border-b p-2"><span class="font-bold mr-3">営業日</span>日曜・祝日と一部の日以外</div>
+        <div class="border-b p-2"><span class="font-bold mr-3">営業日</span>月曜から土曜(日曜・祝日と一部の日は休日)</div>
         <div class="border-b p-2"><span class="font-bold mr-3">営業時間</span>10時から15時</div>
         <div class="p-2"><span class="font-bold mr-3">作業時間</span>営業時間内の一部</div>
         </div>',
