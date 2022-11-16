ca068efb04ca60cef135560ce4f179795662529c
PCS <pcs.engineer.team@gmail.com>
2022-11-16 21:27:20 +0900

    wip

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
     <a href="https://twitter.com/PCS_miraizu" class="text-indigo-500 font-bold underline underline-offset-2"
        target="_blank">Twitter@PCS_miraizu</a>、
     <a href="https://www.instagram.com/pcs_miraizu/" class="text-indigo-500 font-bold underline underline-offset-2"
-       target="_blank">Instagram</a>、
+       target="_blank">Instagram</a>
     等から受け付けています。
 </div>
 <div>
