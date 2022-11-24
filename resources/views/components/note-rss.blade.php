12f141a48d1988db34b2417f3bb490f7e1d0f7d5
PCS <pcs.engineer.team@gmail.com>
2022-11-24 19:02:49 +0900

    Update note-rss.blade.php

--- resources/views/components/note-rss.blade.php
+++ resources/views/components/note-rss.blade.php
@@ -5,7 +5,7 @@
                 <a href="{{ $item['link'] ?? '' }}"
                    class="text-indigo-500 underline underline-offset-2 mr-2"
                    target="_blank">{{ $item['title'] ?? '' }}</a>
-                <time datetime="{{ $item['date'] ?? '' }}T{{ $item['time'] ?? '' }}">{{ $item['diff'] ?? '' }}</time>
+                <time datetime="{{ $item['date'] ?? '' }}T{{ $item['time'] ?? '' }}">{{ $item['date'] ?? '' }}</time>
             </li>
         @endforeach
     </ul>
