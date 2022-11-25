0a01c3b53bb9fc7c53a9cb90628d46aff4668981
PCS <pcs.engineer.team@gmail.com>
2022-11-25 14:48:24 +0900

    Update menu.php

--- config/menu.php
+++ config/menu.php
@@ -6,8 +6,8 @@
  */
 return [
     [
-        'title' => 'Webシステム開発・運営',
-        'text' => '<a href="https://pcs-miraizu.com/" class="underline underline-offset-2" target="_blank">PCSサイト</a>やここのようなプログラマー／エンジニアによるWebシステム開発とその後の継続的な運営。<a href="https://github.com/pop-culture-studio" class="underline underline-offset-2" target="_blank">GitHub</a>
+        'title' => 'Webシステム開発・運用',
+        'text' => '<a href="https://pcs-miraizu.com/" class="underline underline-offset-2" target="_blank">PCSサイト</a>やここのようなプログラマー／エンジニアによるWebシステム開発とその後の継続的な運用。<a href="https://github.com/pop-culture-studio" class="underline underline-offset-2" target="_blank">GitHub</a>
 <div class="bg-white border rounded my-2">
         <div class="border-b p-2"><span class="font-bold mr-3">料金目安</span>月額11万円(10万円+消費税1万円)</div>
         <div class="border-b p-2"><span class="font-bold mr-3">営業日</span>月曜から土曜(日曜・祝日と一部の日は休日)</div>
