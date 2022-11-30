8e616a2c7b49d2963f48450b0897b83f22052c63
PCS <pcs.engineer.team@gmail.com>
2022-11-30 19:48:25 +0900

    Update menu.blade.php

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,15 +1,6 @@
 <div class="mt-2 text-sm text-gray-700">
     <div>こんなことができますという例。</div>
     <div class="divide-y">
-        @foreach (collect(config('menu')) as $menu)
-            <div class="py-3">
-                <h3 class="font-bold text-xl text-gray-900">{{ $menu['title'] ?? '' }}</h3>
-
-                <div class="mt-1">{!! $menu['text'] ?? '' !!}</div>
-            </div>
-        @endforeach
+        @include('menu.develop')
     </div>
-    {{--    <div>--}}
-    {{--        <img src="{{ asset('images/working.jpg') }}" class="w-full shadow-lg rounded-md" alt="仕事一覧" title="仕事一覧">--}}
-    {{--    </div>--}}
 </div>
