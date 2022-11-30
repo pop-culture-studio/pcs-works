0c8e66a299c2a9693dd95a1a2ea0f7ec279b5ab2
PCS <pcs.engineer.team@gmail.com>
2022-11-30 20:29:38 +0900

    Update base-shop.blade.php

--- resources/views/links/base-shop.blade.php
+++ resources/views/links/base-shop.blade.php
@@ -1,13 +1,8 @@
 <div class="py-3">
     <h3 class="font-bold text-xl">
-        <a href="https://sds0301.thebase.in/" target="_blank" class="text-indigo-500 underline underline-offset-2">夢夢堂</a>
+        <a href="https://sds0301.thebase.in/" target="_blank"
+           class="tracking-widest text-[#61278a] underline underline-offset-2 decoration-[#f9dad9]">夢夢堂</a>
     </h3>
-
-{{--    <div>--}}
-{{--        <a href="https://sds0301.thebase.in/" target="_blank">--}}
-{{--            <img src="{{ asset('images/base-shop.png') }}" alt="夢夢堂" title="夢夢堂" width="350" height="121">--}}
-{{--        </a>--}}
-{{--    </div>--}}
 
     <div>ハンドメイド作品を販売するウェブショップ。</div>
 </div>
