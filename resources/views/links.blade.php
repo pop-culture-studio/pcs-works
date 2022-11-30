c009fdb681ee7f72504e39bef3acbc9ef5d1a17c
PCS <pcs.engineer.team@gmail.com>
2022-11-30 14:14:26 +0900

    instagram

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -1,6 +1,7 @@
 <div class="mt-2 text-sm text-gray-700">
     <div class="divide-y">
         @include('links.note')
+        @include('links.instagram')
         @include('links.youtube')
         @include('links.base-shop')
 
---
+++ resources/views/links/instagram.blade.php
@@ -0,0 +1,31 @@
+<div class="py-3">
+    <h3 class="font-bold text-xl">
+        Instagram
+    </h3>
+
+    <ul class="p-2 space-y-1">
+        <li>
+            <a href="https://www.instagram.com/pcs_miraizu/"
+               class="text-indigo-500 underline underline-offset-2"
+               target="_blank">
+                PCS未来図
+            </a>
+        </li>
+
+        <li>
+            <a href="https://www.instagram.com/miraizu_souko/"
+               class="text-indigo-500 underline underline-offset-2"
+               target="_blank">
+                未来図倉庫
+            </a>
+        </li>
+
+        <li>
+            <a href="https://www.instagram.com/pcs_puramobu/"
+               class="text-indigo-500 underline underline-offset-2"
+               target="_blank">
+                PCSプラモ部
+            </a>
+        </li>
+    </ul>
+</div>
