4d2f2b95b1a72596de523110b12ad021f4b93571
PCS <pcs.engineer.team@gmail.com>
2022-11-30 21:14:33 +0900

    Update souko.blade.php

--- resources/views/works/souko.blade.php
+++ resources/views/works/souko.blade.php
@@ -5,7 +5,7 @@
     </h3>
 
     <div>フリー素材サイト。</div>
-    <div class="bg-white p-1 rounded-md">
+    <div class="bg-white p-1 rounded-md border">
         <a href="https://ggijp-pcs.com/" target="_blank">
             <img src="{{ asset('images/souko_logo.png') }}" alt="未来図倉庫" title="未来図倉庫">
         </a>
