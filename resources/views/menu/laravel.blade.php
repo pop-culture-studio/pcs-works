616605a2a04af8579695c79a0428f634bd43c98d
PCS <pcs.engineer.team@gmail.com>
2022-11-30 19:56:19 +0900

    Create laravel.blade.php

---
+++ resources/views/menu/laravel.blade.php
@@ -0,0 +1,10 @@
+<div class="py-3">
+    <h3 class="font-bold text-xl text-gray-900">Laravelプロジェクトの開発サポート</h3>
+
+    <div class="mt-1">
+        既存のLaravelプロジェクトへの開発サポート。
+        <div class="bg-white border rounded my-2">
+            <div class="p-2"><span class="font-bold mr-3">料金目安</span>要見積</div>
+        </div>
+    </div>
+</div>
