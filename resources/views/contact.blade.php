c2b4bb55cfa1f4d0f7558023dacd4da024c3e5e8
PCS <pcs.engineer.team@gmail.com>
2022-11-30 20:54:53 +0900

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,10 +1,4 @@
 <div class="py-3 text-lg">
-    問い合わせは
-    <a href="https://pcs-miraizu.com/" class="text-indigo-500 font-bold underline underline-offset-2"
-       target="_blank">PCS公式サイト</a>、
-    <a href="https://twitter.com/PCS_miraizu" class="text-indigo-500 font-bold underline underline-offset-2"
-       target="_blank">Twitter@PCS_miraizu</a>、
-    <a href="https://www.instagram.com/pcs_miraizu/" class="text-indigo-500 font-bold underline underline-offset-2"
-       target="_blank">Instagram</a>
-    等から受け付けています。
+    <a href="mailto:pcs_fukuoka@pfijp.com"
+       class="text-indigo-500 font-bold underline underline-offset-2">メールで問い合わせる</a>
 </div>
