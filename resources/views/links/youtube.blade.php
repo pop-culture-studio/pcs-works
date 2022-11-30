a1e4fb985260987273cce7d24ce73653e2e7932e
PCS <pcs.engineer.team@gmail.com>
2022-11-30 20:23:22 +0900

    Update youtube.blade.php

--- resources/views/links/youtube.blade.php
+++ resources/views/links/youtube.blade.php
@@ -3,5 +3,5 @@
         <a href="https://www.youtube.com/channel/UCGlJLfwU0Sl80kPhYxd9IIg" target="_blank" class="text-indigo-500 underline underline-offset-2">未来図チャンネル</a>
     </h3>
 
-    <div>YouTubeチャンネル。</div>
+    <div class="mt-1"><span class="font-bold bg-red-500 text-white rounded p-1">YouTube</span>チャンネル。</div>
 </div>
