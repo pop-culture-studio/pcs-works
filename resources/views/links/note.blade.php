3225ecd04417fb3cb96922b1e31dc66945e4c5af
PCS <pcs.engineer.team@gmail.com>
2022-11-30 20:23:18 +0900

    Update note.blade.php

--- resources/views/links/note.blade.php
+++ resources/views/links/note.blade.php
@@ -1,6 +1,6 @@
 <div class="py-3">
     <h3 class="font-bold text-xl">
-        <a href="https://note.com/pcs_miraizu" target="_blank" class="text-indigo-500 underline underline-offset-2">PCS未来図 | note</a>
+        <a href="https://note.com/pcs_miraizu" target="_blank" class="text-[#41C9B4] underline  underline-offset-2">PCS未来図 | note</a>
     </h3>
 
     <div>メンバーブログ。</div>
