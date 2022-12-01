934a632f156af734c1bc55d7752e48c0a4016ff4
PCS <pcs.engineer.team@gmail.com>
2022-12-01 09:11:59 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,34 +20,16 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;600;700;800;900&display=swap">
 
-        {{-- @livewireStyles --}}
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        {{-- <x-jet-banner /> --}}
-
         <div class="min-h-screen bg-gray-100">
-            {{-- @livewire('navigation-menu') --}}
-
-            <!-- Page Heading -->
-            {{-- @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif --}}
-
-            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
 
         @stack('modals')
-
-        {{-- @livewireScripts --}}
     </body>
 </html>
