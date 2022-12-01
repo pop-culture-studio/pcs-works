7818baf860b003a89b32f8cba49e031af771e449
PCS <pcs.engineer.team@gmail.com>
2022-12-01 09:07:52 +0900

    wip

--- resources/views/menu/aws.blade.php
+++ resources/views/menu/aws.blade.php
@@ -4,7 +4,7 @@
     <div class="mt-1">
         「インフラエンジニアがいないのでサポートして欲しい」や「稼働中のシステムをAWSに移行したい」など。
         <div class="bg-white border rounded my-2">
-            <div class="p-2"><span class="font-bold mr-3">料金目安</span>要見積</div>
+            <div class="p-2"><span class="font-bold mr-3">料金目安</span>要見積。月額制。</div>
         </div>
     </div>
 </div>
--- resources/views/menu/laravel.blade.php
+++ resources/views/menu/laravel.blade.php
@@ -4,7 +4,7 @@
     <div class="mt-1">
         既存のLaravelプロジェクトへの開発サポート。
         <div class="bg-white border rounded my-2">
-            <div class="p-2"><span class="font-bold mr-3">料金目安</span>要見積</div>
+            <div class="p-2"><span class="font-bold mr-3">料金目安</span>要見積。月額制。</div>
         </div>
     </div>
 </div>
