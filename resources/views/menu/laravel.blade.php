274e8cffa82a8d2b6575e641826dc569bdf2be15
pop-culture-studio <pcs.engineer.team@gmail.com>
2022-12-19 20:46:00 +0900

    Update laravel.blade.php

--- resources/views/menu/laravel.blade.php
+++ resources/views/menu/laravel.blade.php
@@ -2,7 +2,7 @@
     <h3 class="font-bold text-xl text-gray-900">Laravelプロジェクトの開発サポート</h3>
 
     <div class="mt-1">
-        既存のLaravelプロジェクトへの開発サポート。
+        既存のLaravelプロジェクトへの開発サポート。「開発・運用中のサービスへの協力」や「どこかに依頼して作ったものの現在は誰もメンテできないまま使っているシステムの引継ぎ」
         <div class="bg-white border rounded my-2">
             <div class="p-2"><span class="font-bold mr-3">料金目安</span>要見積。月額制。</div>
         </div>
