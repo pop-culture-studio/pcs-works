4f304537337e4161c21f4ab28e1edd6ae1ca032d
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-01-11 14:31:00 +0900

    Update laravel.blade.php

--- resources/views/menu/laravel.blade.php
+++ resources/views/menu/laravel.blade.php
@@ -2,7 +2,7 @@
     <h3 class="font-bold text-xl text-gray-900">Laravelプロジェクトの開発引き継ぎ</h3>
 
     <div class="mt-1">
-        既存のLaravelプロジェクトの引き継ぎ。「どこかに依頼して作ったものの現在は誰もメンテできないまま使っているシステムの引き継ぎ」や「古いバージョンのまま動いているLaravelやPHPのバージョンアップ」など。
+        既存のLaravelプロジェクトの引き継ぎ。「どこかに依頼して作ったものの現在は誰もメンテできないまま使っているシステムの引き継ぎ」や「古いバージョンのまま動いているLaravelやPHPのバージョンアップ」など。完全な引き継ぎのみ受付。
         <div class="bg-white border rounded my-2">
             <div class="p-2"><span class="font-bold mr-3">料金目安</span>要見積。月額制。</div>
         </div>
