73a27f0b27187471cd44a212764250ebfebe2f07
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-02-15 11:07:10 +0900

    Laravel10

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout>
+    <div class="max-w-7xl mx-auto">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            @include('welcome')
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'dashboard');
+Route::view('/', 'index');
+
+//Route::middleware([
+//    'auth:sanctum',
+//    config('jetstream.auth_session'),
+//    'verified'
+//])->group(function () {
+//    Route::get('/dashboard', function () {
+//        return view('dashboard');
+//    })->name('dashboard');
+//});
