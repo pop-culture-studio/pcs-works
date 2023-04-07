848f0c42daab8278402c93bd285f8d3be32e097f
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-04-07 12:52:47 +0900

    Update books.php

--- config/books.php
+++ config/books.php
@@ -2,6 +2,10 @@
 
 return [
     [
+        'title' => 'Laravel再入門 Vol.3 ChatGPTが使えるLINE botを作る',
+        'url' => 'https://zenn.dev/pcs_engineer/books/re-laravel-3',
+    ],
+    [
         'title' => 'Laravel再入門 Vol.2 ChatGPTでLaravelのTIPSを生成してDiscordやNostrに投稿するbotを作る',
         'url' => 'https://zenn.dev/pcs_engineer/books/re-laravel-2',
     ],
